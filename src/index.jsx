ab36dd50643738aa76ce4d404a68f84e1c151120
superlucky19971023 <superlucky19971023@gmail.com>
2024-04-10 14:41:41 -0300

    change

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 import {
-  APP_INIT_ERROR, APP_READY, subscribe,
+  APP_INIT_ERROR, APP_READY, subscribe, initialize,
 } from '@edx/frontend-platform';
 import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
 import ReactDOM from 'react-dom';
@@ -29,4 +29,6 @@ subscribe(APP_INIT_ERROR, (error) => {
   ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
 });
 
-
+initialize({
+  messages,
+});
