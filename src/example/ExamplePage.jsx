1d9ffe658810c82c765845b5700762cb0f755f3a
superlucky19971023 <superlucky19971023@gmail.com>
2024-04-11 02:10:34 -0300

    new footer header

--- src/example/ExamplePage.jsx
+++ src/example/ExamplePage.jsx
@@ -1,14 +1,10 @@
 import { Container } from '@openedx/paragon';
-import Header from '../components/Header/Header';
-import Footer from '../components/Footer/Footer';
 
 const ExamplePage = () => (
   <main>
     <Container className="py-5">
-      <Header />
       <h1>Welcome</h1>
       <p>to PANORAMA by Aulasneo</p>
-      <Footer />
     </Container>
   </main>
 );
--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,16 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import { AppProvider } from '@edx/frontend-platform/react';
 import ReactDOM from 'react-dom';
-import Header from '@edx/frontend-component-header';
-import Footer from '@edx/frontend-component-footer';
-
 import ExamplePage from './example/ExamplePage';
-
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import './index.scss';
 
 ReactDOM.render(
-  <AppProvider>
+  <>
     <Header />
     <ExamplePage />
     <Footer />
-  </AppProvider>,
+  </>,
   document.getElementById('root'),
 );
