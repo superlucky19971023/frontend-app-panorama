b3717ceaa0b702d23174f93106a43427136ce0d0
superlucky19971023 <superlucky19971023@gmail.com>
2024-04-11 01:27:36 -0300

    appprovider

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import { AppProvider } from '@edx/frontend-platform/react';
 import ReactDOM from 'react-dom';
 import ExamplePage from './example/ExamplePage';
 import Header from './components/Header/Header';
@@ -7,10 +8,10 @@ import Footer from './components/Footer/Footer';
 import './index.scss';
 
 ReactDOM.render(
-  <>
+  <AppProvider>
     <Header />
     <ExamplePage />
     <Footer />
-  </>,
+  </AppProvider>,
   document.getElementById('root'),
 );
