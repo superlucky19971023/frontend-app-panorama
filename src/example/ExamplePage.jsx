b671ccf4419468ee523a984f53f6522a7f3faa8c
superlucky19971023 <superlucky19971023@gmail.com>
2024-04-11 01:56:22 -0300

    new footer header

--- src/example/ExamplePage.jsx
+++ src/example/ExamplePage.jsx
@@ -1,6 +1,6 @@
 import { Container } from '@openedx/paragon';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
 
 const ExamplePage = () => (
   <main>
--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,11 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { AppProvider } from '@edx/frontend-platform/react';
 import ReactDOM from 'react-dom';
-import ExamplePage from './example/ExamplePage';
 import Header from '@edx/frontend-component-header';
 import Footer from '@edx/frontend-component-footer';
+
+import ExamplePage from './example/ExamplePage';
+
 import './index.scss';
 
 ReactDOM.render(
