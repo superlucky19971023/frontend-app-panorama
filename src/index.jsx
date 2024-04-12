4920912b849a953b38e9a3289dafd2b9a2e930d9
superlucky19971023 <superlucky19971023@gmail.com>
2024-04-12 13:10:14 -0300

    index

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import {
-  APP_INIT_ERROR, APP_READY, subscribe, initialize,
+  APP_READY, subscribe, initialize,
   mergeConfig,
 } from '@edx/frontend-platform';
 
@@ -26,42 +26,10 @@ subscribe(APP_READY, () => {
   );
 });
 
-subscribe(APP_INIT_ERROR, (error) => {
-  ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
-});
+// subscribe(APP_INIT_ERROR, (error) => {
+//   ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
+// });
 
 initialize({
-  handlers: {
-    config: () => {
-      mergeConfig({
-        AI_TRANSLATIONS_URL: process.env.AI_TRANSLATIONS_URL || null,
-        CONTACT_URL: process.env.CONTACT_URL || null,
-        CREDENTIALS_BASE_URL: process.env.CREDENTIALS_BASE_URL || null,
-        CREDIT_HELP_LINK_URL: process.env.CREDIT_HELP_LINK_URL || null,
-        DISCUSSIONS_MFE_BASE_URL: process.env.DISCUSSIONS_MFE_BASE_URL || null,
-        ENTERPRISE_LEARNER_PORTAL_HOSTNAME: process.env.ENTERPRISE_LEARNER_PORTAL_HOSTNAME || null,
-        ENABLE_JUMPNAV: process.env.ENABLE_JUMPNAV || null,
-        ENABLE_NEW_SIDEBAR: process.env.ENABLE_NEW_SIDEBAR || null,
-        ENABLE_NOTICES: process.env.ENABLE_NOTICES || null,
-        INSIGHTS_BASE_URL: process.env.INSIGHTS_BASE_URL || null,
-        SEARCH_CATALOG_URL: process.env.SEARCH_CATALOG_URL || null,
-        SOCIAL_UTM_MILESTONE_CAMPAIGN: process.env.SOCIAL_UTM_MILESTONE_CAMPAIGN || null,
-        STUDIO_BASE_URL: process.env.STUDIO_BASE_URL || null,
-        SUPPORT_URL: process.env.SUPPORT_URL || null,
-        SUPPORT_URL_CALCULATOR_MATH: process.env.SUPPORT_URL_CALCULATOR_MATH || null,
-        SUPPORT_URL_ID_VERIFICATION: process.env.SUPPORT_URL_ID_VERIFICATION || null,
-        SUPPORT_URL_VERIFIED_CERTIFICATE: process.env.SUPPORT_URL_VERIFIED_CERTIFICATE || null,
-        TERMS_OF_SERVICE_URL: process.env.TERMS_OF_SERVICE_URL || null,
-        TWITTER_HASHTAG: process.env.TWITTER_HASHTAG || null,
-        TWITTER_URL: process.env.TWITTER_URL || null,
-        LEGACY_THEME_NAME: process.env.LEGACY_THEME_NAME || null,
-        EXAMS_BASE_URL: process.env.EXAMS_BASE_URL || null,
-        PROCTORED_EXAM_FAQ_URL: process.env.PROCTORED_EXAM_FAQ_URL || null,
-        PROCTORED_EXAM_RULES_URL: process.env.PROCTORED_EXAM_RULES_URL || null,
-        CHAT_RESPONSE_URL: process.env.CHAT_RESPONSE_URL || null,
-        PRIVACY_POLICY_URL: process.env.PRIVACY_POLICY_URL || null,
-      }, 'LearnerAppConfig');
-    },
-  },
   messages,
 });
