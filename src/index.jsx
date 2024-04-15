af758fe74056aa6c675801d520f64d803ab55bf1
superlucky19971023 <superlucky19971023@gmail.com>
2024-04-15 15:18:08 -0300

    a

--- src/index.jsx
+++ src/index.jsx
@@ -1,41 +1,70 @@
+ import 'core-js/stable';
+ import 'regenerator-runtime/runtime';
+ import {
+   APP_INIT_ERROR, APP_READY, subscribe, initialize,
+   mergeConfig,
+ } from '@edx/frontend-platform';
+ import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
+ import ReactDOM from 'react-dom';
+ import Header from '@edx/frontend-component-header';
+ import Footer from '@edx/frontend-component-footer';
+ import messages from './i18n';
+ import AppPanorama from './AppPanorama';
+ import './index.scss';
+ subscribe(APP_READY, () => {
+   ReactDOM.render(
+     <AppProvider>
+       <Header />
+       <AppPanorama />
+       <Footer />
+     </AppProvider>,
+     document.getElementById('root'),
+   );
+ });
+ subscribe(APP_INIT_ERROR, (error) => {
+   ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
+ });
+ initialize({
+   messages,
+ });
+ 
+//  import 'core-js/stable';
+// import 'regenerator-runtime/runtime';
+// import ReactDOM from 'react-dom';
+// import {
+//   APP_INIT_ERROR, APP_READY, subscribe, initialize,
+//   mergeConfig,
+// } from '@edx/frontend-platform';
+// import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
+// import Header from '@edx/frontend-component-header';
+// import Footer from '@edx/frontend-component-footer';
+// import AppPanorama from './AppPanorama';
 
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-import ReactDOM from 'react-dom';
-import {
-  APP_INIT_ERROR, APP_READY, subscribe, initialize,
-  mergeConfig,
-} from '@edx/frontend-platform';
-import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
-import Header from '@edx/frontend-component-header';
-import Footer from '@edx/frontend-component-footer';
-import AppPanorama from './AppPanorama';
+// import { IntlProvider } from 'react-intl';
+// import './index.scss';
 
-import { IntlProvider } from 'react-intl';
-import './index.scss';
+// const messages = {
+//   en: {
+//     someMessageId: 'Hello, World!',
+//   },
+// };
 
-const messages = {
-  en: {
-    someMessageId: 'Hello, World!',
-  },
-};
+// ReactDOM.render(
 
-ReactDOM.render(
+//   <IntlProvider locale="en" messages={messages.en}>
+//     <AppProvider>
+//       <Header />
+//       <AppPanorama />
+//       <Footer />
+//     </AppProvider>
+//   </IntlProvider>,
+//   document.getElementById('root'),
+// );
 
-  <IntlProvider locale="en" messages={messages.en}>
-    
-      <Header />
-      <AppPanorama />
-      <Footer />
-    
-  </IntlProvider>,
-  document.getElementById('root'),
-);
+// // subscribe(APP_INIT_ERROR, (error) => {
+// //   ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
+// // });
 
-// subscribe(APP_INIT_ERROR, (error) => {
-//   ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
+// initialize({
+//   messages,
 // });
-
-initialize({
-  messages,
-});
