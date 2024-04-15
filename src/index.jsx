d8186e23f19fd6197a42eb229d018bc265eea0bf
superlucky19971023 <superlucky19971023@gmail.com>
2024-04-14 23:26:55 -0300

    aaa

--- src/index.jsx
+++ src/index.jsx
@@ -1,39 +1,3 @@
-// import 'core-js/stable';
-// import 'regenerator-runtime/runtime';
-// import {
-//   APP_INIT_ERROR, APP_READY, subscribe, initialize,
-//   mergeConfig,
-// } from '@edx/frontend-platform';
-
-// import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
-// import ReactDOM from 'react-dom';
-
-// import Header from '@edx/frontend-component-header';
-// import Footer from '@edx/frontend-component-footer';
-// import messages from './i18n';
-// import AppPanorama from './AppPanorama';
-
-// import './index.scss';
-
-// subscribe(APP_READY, () => {
-//   ReactDOM.render(
-//     <AppProvider>
-//       <Header />
-//       <AppPanorama />
-//       <Footer />
-//     </AppProvider>,
-//     document.getElementById('root'),
-//   );
-// });
-
-// subscribe(APP_INIT_ERROR, (error) => {
-//   ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
-// });
-
-// initialize({
-//   messages,
-// });
-
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import ReactDOM from 'react-dom';
@@ -58,11 +22,11 @@ const messages = {
 ReactDOM.render(
 
   <IntlProvider locale="en" messages={messages.en}>
-    <AppProvider>
+   
       <Header />
       <AppPanorama />
       <Footer />
-    </AppProvider>Holaaa
+   
   </IntlProvider>,
   document.getElementById('root'),
 );
