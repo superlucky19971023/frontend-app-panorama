7a3e3af662b36d58789706097b9e59a93d4d4045
superlucky19971023 <superlucky19971023@gmail.com>
2024-04-15 15:42:03 -0300

    a

--- src/index.jsx
+++ src/index.jsx
@@ -1,34 +1,36 @@
- import 'core-js/stable';
- import 'regenerator-runtime/runtime';
- import {
-   APP_INIT_ERROR, APP_READY, subscribe, initialize,
-   mergeConfig,
- } from '@edx/frontend-platform';
- import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
- import ReactDOM from 'react-dom';
- import Header from '@edx/frontend-component-header';
- import Footer from '@edx/frontend-component-footer';
- import messages from './i18n';
- import AppPanorama from './AppPanorama';
- import './index.scss';
- subscribe(APP_READY, () => {
-   ReactDOM.render(
-     <AppProvider>
-       <Header />
-       <AppPanorama />
-       <Footer />
-     </AppProvider>,
-     document.getElementById('root'),
-   );
- });
- subscribe(APP_INIT_ERROR, (error) => {
-   ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
- });
- initialize({
-   messages,
- });
- 
-//  import 'core-js/stable';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import {
+  APP_INIT_ERROR, APP_READY, subscribe, initialize,
+} from '@edx/frontend-platform';
+import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
+import ReactDOM from 'react-dom';
+import Header from '@edx/frontend-component-header';
+import Footer from '@edx/frontend-component-footer';
+import messages from './i18n';
+import AppPanorama from './AppPanorama';
+import './index.scss';
+subscribe(APP_READY, () => {
+
+  console.log("subscribe", APP_INIT_ERROR, APP_READY, subscribe, initialize)
+
+  ReactDOM.render(
+    <>
+      <Header />
+      <AppPanorama />
+      <Footer />
+    </>,
+    document.getElementById('root'),
+  );
+});
+  subscribe(APP_INIT_ERROR, (error) => {
+    ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
+  });
+initialize({
+  messages,
+});
+
+// import 'core-js/stable';
 // import 'regenerator-runtime/runtime';
 // import ReactDOM from 'react-dom';
 // import {
