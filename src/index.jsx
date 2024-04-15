95f2fd1465cf02f9561606204b9a4b6bdc2afbd6
superlucky19971023 <superlucky19971023@gmail.com>
2024-04-15 11:40:22 -0300

    a

--- src/index.jsx
+++ src/index.jsx
@@ -1,69 +1,40 @@
-// import 'core-js/stable';
-// import 'regenerator-runtime/runtime';
-// import ReactDOM from 'react-dom';
-// import {
-//   APP_INIT_ERROR, APP_READY, subscribe, initialize,
-//   mergeConfig,
-// } from '@edx/frontend-platform';
-// import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
-// import Header from '@edx/frontend-component-header';
-// import Footer from '@edx/frontend-component-footer';
-// import AppPanorama from './AppPanorama';
-
-// import { IntlProvider } from 'react-intl';
-// import './index.scss';
-
-
-// ReactDOM.render(
-
-//   <IntlProvider locale="en" messages={messages.en}>
-//     <AppProvider>
-//       <Header />
-//       <AppPanorama />
-//       <Footer />
-//     </AppProvider>
-//   </IntlProvider>,
-//   document.getElementById('root'),
-// );
-
-// subscribe(APP_INIT_ERROR, (error) => {
-//    ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
-// });
-
-// initialize({
-//   messages,
-// });
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-
+import ReactDOM from 'react-dom';
 import {
   APP_INIT_ERROR, APP_READY, subscribe, initialize,
+  mergeConfig,
 } from '@edx/frontend-platform';
 import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
-import ReactDOM from 'react-dom';
-
 import Header from '@edx/frontend-component-header';
 import Footer from '@edx/frontend-component-footer';
-import messages from './i18n';
-
-import './index.scss';
 import AppPanorama from './AppPanorama';
 
-subscribe(APP_READY, () => {
-  ReactDOM.render(
-    <AppProvider>
+import { IntlProvider } from 'react-intl';
+import './index.scss';
+
+const messages = {
+  en: {
+    someMessageId: 'Hello, World!',
+  },
+};
+
+ReactDOM.render(
+
+  <IntlProvider locale="en" messages={messages.en}>
+    
       <Header />
       <AppPanorama />
       <Footer />
-    </AppProvider>,
-    document.getElementById('root'),
-  );
-});
+    
+  </IntlProvider>,
+  document.getElementById('root'),
+);
 
-subscribe(APP_INIT_ERROR, (error) => {
-  ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
-});
+// subscribe(APP_INIT_ERROR, (error) => {
+//   ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
+// });
 
 initialize({
   messages,
