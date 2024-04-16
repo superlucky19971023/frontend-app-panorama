137bdb014bf95373246157b297974b74150ab3c9
superlucky19971023 <superlucky19971023@gmail.com>
2024-04-16 14:12:48 -0300

    aaaa

--- src/index.jsx
+++ src/index.jsx
@@ -1,33 +1,37 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-import ReactDOM from 'react-dom';
-import {
-  APP_INIT_ERROR, APP_READY, subscribe, initialize,
-  mergeConfig,
-} from '@edx/frontend-platform';
-import { AppProvider } from "@edx/frontend-platform/react/AppProvider"
-import Header from '@edx/frontend-component-header';
-import Footer from '@edx/frontend-component-footer';
-import AppPanorama from './example/data/AppPanorama'
-import { IntlProvider } from 'react-intl';
-import './index.scss'
-import AppContext from '@edx/frontend-platform/react/AppContext';
-const messages = {
-  en: {
-    someMessageId: 'Hello, World!',
-  },
-}
-ReactDOM.render(
-  <IntlProvider locale="en" messages={messages.en}>
-    <AppContext>
-      <Header />
-      <AppPanorama />
-      <Footer />
-    </AppContext>
-  </IntlProvider>,
-  document.getElementById('root'),
+//  import 'core-js/stable';
+//  import 'regenerator-runtime/runtime';
+//  import ReactDOM from 'react-dom';
+//  import {
+//    APP_INIT_ERROR, APP_READY, subscribe, initialize,
+//    mergeConfig,
+//  } from '@edx/frontend-platform';
+//  import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
+//  import Header from '@edx/frontend-component-header';
+//  import Footer from '@edx/frontend-component-footer';
+//  import AppPanorama from './AppPanorama';
 
-);
+//  import { IntlProvider } from 'react-intl';
+//  import './index.scss';
+
+//  const messages = {
+//    en: {
+//      someMessageId: 'Hello, World!',
+//    },
+//  };
+
+//  ReactDOM.render(
+
+//    <IntlProvider locale="en" messages={messages.en}>
+//      <AppProvider>
+//        <Header />
+//        <AppPanorama />
+//        <Footer />
+//      </AppProvider>
+//    </IntlProvider>,
+//    document.getElementById('root'),
+
+//    console.log("subscribe", AppProvider)
+//  );
 //  console.log("subscribe", AppProvider)
 // subscribe(APP_INIT_ERROR, (error) => {
 //   ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
@@ -37,32 +41,32 @@ ReactDOM.render(
 //   messages,
 // });
 
-// import 'core-js/stable';
-// import 'regenerator-runtime';
+import 'core-js/stable';
+import 'regenerator-runtime';
 
-// import {
-//   APP_INIT_ERROR, APP_READY, subscribe, initialize,
-// } from '@edx/frontend-platform';
-// import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
-// import ReactDOM from 'react-dom';
+import {
+  APP_INIT_ERROR, APP_READY, subscribe, initialize,
+} from '@edx/frontend-platform';
+import { AppContext, AppProvider, ErrorPage } from '@edx/frontend-platform/react';
+import ReactDOM from 'react-dom';
 
-// import Header from '@edx/frontend-component-header';
-// import Footer from '@edx/frontend-component-footer';
-// import messages from './i18n';
-// import AppPanorama from './example/AppPanorama';
+import Header from '@edx/frontend-component-header';
+import Footer from '@edx/frontend-component-footer';
+import messages from './i18n';
+import AppPanorama from './example/data/AppPanorama';
 
-// import './index.scss';
+import './index.scss';
 
-// subscribe(APP_READY, () => {
-//   ReactDOM.render(
-//     <AppProvider>
-//       <Header />
-//       <AppPanorama />
-//       <Footer />
-//     </AppProvider>,
-//     document.getElementById('root'),
-//   );
-// });
+subscribe(APP_READY, () => {
+  ReactDOM.render(
+    <AppContext>
+      <Header />
+      <AppPanorama />
+      <Footer />
+    </AppContext>,
+    document.getElementById('root'),
+  );
+});
 
 // subscribe(APP_INIT_ERROR, (error) => {
 //   ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
