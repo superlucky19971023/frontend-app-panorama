a698e82f55923ce1119828c77da42f0deff41509
superlucky19971023 <superlucky19971023@gmail.com>
2024-04-16 13:49:50 -0300

    aaa

--- src/index.jsx
+++ src/index.jsx
@@ -1,32 +1,33 @@
-   import 'core-js/stable';
-   import 'regenerator-runtime/runtime';
-   import ReactDOM from 'react-dom';
-   import {
-     APP_INIT_ERROR, APP_READY, subscribe, initialize,
-     mergeConfig,
-   } from '@edx/frontend-platform';
-   import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
-   import Header from '@edx/frontend-component-header';
-   import Footer from '@edx/frontend-component-footer';
-   import AppPanorama from './example/data/AppPanorama' 
-   import { IntlProvider } from 'react-intl';
-   import './index.scss' 
-   const messages = {
-     en: {
-       someMessageId: 'Hello, World!',
-     },
-   } 
-   ReactDOM.render (
-     <IntlProvider locale="en" messages={messages.en}>
-       <AppProvider>
-         <Header />
-         <AppPanorama />
-         <Footer />
-       </AppProvider>
-     </IntlProvider>,
-     document.getElementById('root'),
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import ReactDOM from 'react-dom';
+import {
+  APP_INIT_ERROR, APP_READY, subscribe, initialize,
+  mergeConfig,
+} from '@edx/frontend-platform';
+import { AppProvider } from "@edx/frontend-platform/react/AppProvider"
+import Header from '@edx/frontend-component-header';
+import Footer from '@edx/frontend-component-footer';
+import AppPanorama from './example/data/AppPanorama'
+import { IntlProvider } from 'react-intl';
+import './index.scss'
+import AppContext from '@edx/frontend-platform/react/AppContext';
+const messages = {
+  en: {
+    someMessageId: 'Hello, World!',
+  },
+}
+ReactDOM.render(
+  <IntlProvider locale="en" messages={messages.en}>
+    <AppContext>
+      <Header />
+      <AppPanorama />
+      <Footer />
+    </AppContext>
+  </IntlProvider>,
+  document.getElementById('root'),
 
-  );
+);
 //  console.log("subscribe", AppProvider)
 // subscribe(APP_INIT_ERROR, (error) => {
 //   ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
