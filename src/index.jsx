0da946f349100211a950bf422a7f504998ef1e20
superlucky19971023 <superlucky19971023@gmail.com>
2024-04-16 17:48:53 -0300

    aaa

--- src/index.jsx
+++ src/index.jsx
@@ -1,41 +1,48 @@
+//  import 'core-js/stable';
+//  import 'regenerator-runtime/runtime';
+//  import ReactDOM from 'react-dom';
+//  import {
+//    APP_INIT_ERROR, APP_READY, subscribe, initialize,
+//    mergeConfig,
+//  } from '@edx/frontend-platform';
+//  import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
+//  import Header from '@edx/frontend-component-header';
+//  import Footer from '@edx/frontend-component-footer';
+//  import AppPanorama from './AppPanorama';
 
-// import 'core-js/stable';
-// import 'regenerator-runtime';
+//  import { IntlProvider } from 'react-intl';
+//  import './index.scss';
 
-// import {
-//   APP_INIT_ERROR, APP_READY, subscribe, initialize,
-// } from '@edx/frontend-platform';
-// import { AppContext, AppProvider, ErrorPage } from '@edx/frontend-platform/react';
-// import ReactDOM from 'react-dom';
+//  const messages = {
+//    en: {
+//      someMessageId: 'Hello, World!',
+//    },
+//  };
 
-// import Header from '@edx/frontend-component-header';
-// import Footer from '@edx/frontend-component-footer';
-// import messages from './i18n';
-// import AppPanorama from './example/data/AppPanorama';
+//  ReactDOM.render(
 
-// import './index.scss';
+//    <IntlProvider locale="en" messages={messages.en}>
+//      <AppProvider>
+//        <Header />
+//        <AppPanorama />
+//        <Footer />
+//      </AppProvider>
+//    </IntlProvider>,
+//    document.getElementById('root'),
 
-// subscribe(APP_READY, () => {
-//   ReactDOM.render(
-//     <>
-//       <Header />
-//       <AppPanorama />
-//       <Footer />
-//     </>,
-//     document.getElementById('root'),
-//   );
+//    console.log("subscribe", AppProvider)
+//  );
+//  console.log("subscribe", AppProvider)
+// subscribe(APP_INIT_ERROR, (error) => {
+//   ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
 // });
 
-// // subscribe(APP_INIT_ERROR, (error) => {
-// //   ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
-// // });
-
-// // initialize({
-// //   messages,
-// // });
+// initialize({
+//   messages,
+// });
 
 import 'core-js/stable';
-import 'regenerator-runtime/runtime';
+import 'regenerator-runtime';
 
 import {
   APP_INIT_ERROR, APP_READY, subscribe, initialize,
@@ -68,3 +75,4 @@ subscribe(APP_INIT_ERROR, (error) => {
 initialize({
   messages,
 });
+
