f9bd107d52e43a73cdfcfb57a1ee4accd693dc7f
superlucky19971023 <superlucky19971023@gmail.com>
2024-06-03 12:13:06 -0300

    add diferent homes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,50 +1,31 @@
 import React, { useContext } from 'react';
-import { useTranslation } from 'react-i18next';
-import CircularProgress from '@mui/material/CircularProgress';
 import { DashboardTypeContext } from '../DashboardContext';
-import Tabs from '../Tabs/Tabs';
-import Embed from '../Embed';
+import EmbedHome from './EmbedHome';
 
 const Home = () => {
-  const {
-    dashboardType, loader, error, response,
-  } = useContext(DashboardTypeContext);
-  const { t } = useTranslation(['global']);
+    const {
+        homeMode
+    } = useContext(DashboardTypeContext);
 
-  return (
-    <div className="dashboard" id="dashboard">
-      {!error && <Embed />}
-      <Tabs />
-      {!error && loader ? (
-        <div className="circularProgress">
-          <CircularProgress />
+    console.log(homeMode)
+    return (
+        <div className="dashboard" id="dashboard">
+            <EmbedHome />
+            <div className="framesContainerHome" id="framesContainerHome">
+                {
+                    (homeMode === 'FREE') ? (
+                        <iframe className="homes-iframe" src="https://panorama-home-pages.s3.amazonaws.com/panorama-free-home.html"></iframe>
+                    ) : (homeMode === 'SAAS') ? (
+                        <iframe className="homes-iframe" src="https://panorama-home-pages.s3.amazonaws.com/panorama-saas-home.html"></iframe>
+                    ) : (homeMode === 'CUSTOM') ? (
+                        <iframe className="homes-iframe" src="https://panorama-home-pages.s3.amazonaws.com/panorama-custom-home.html"></iframe>
+                    ) : (
+                        <iframe className="homes-iframe" src="https://panorama-home-pages.s3.amazonaws.com/panorama-demo-home.html"></iframe>
+                    )
+                }
+            </div>
         </div>
-      ) : (
-        <div className="framesContainer" id="framesContainer">
-          {response && response.map((item) => (
-            <div
-              key={item.name}
-              style={{
-                width: '100%',
-                display: dashboardType === item.displayName ? 'flex' : 'none',
-              }}
-              id={`${item.name}Container`}
-            />
-          ))}
-        </div>
-      )}
-      {error && (
-        <div className="home">
-          <div className="errorMessage">
-            <p>Error: {error} {t('errors.messageError')}</p>
-            <a className="button-contact" href="/">
-              {t('errors.btnBack')}
-            </a>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default Home;
