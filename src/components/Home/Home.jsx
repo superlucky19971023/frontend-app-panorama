cbf5458a959b9b693b4e1e46d87baf40dea0140c
superlucky19971023 <superlucky19971023@gmail.com>
2024-06-06 11:57:14 -0300

    fix code errors

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,31 +5,22 @@ import EmbedHome from './EmbedHome';
 const Home = () => {
   const { homeMode } = useContext(DashboardTypeContext);
 
+  let iframeSrc;
+  if (homeMode === 'FREE') {
+    iframeSrc = 'https://panorama-home-pages.s3.amazonaws.com/panorama-free-home.html';
+  } else if (homeMode === 'SAAS') {
+    iframeSrc = 'https://panorama-home-pages.s3.amazonaws.com/panorama-saas-home.html';
+  } else if (homeMode === 'CUSTOM') {
+    iframeSrc = 'https://panorama-home-pages.s3.amazonaws.com/panorama-custom-home.html';
+  } else {
+    iframeSrc = 'https://panorama-home-pages.s3.amazonaws.com/panorama-demo-home.html';
+  }
+
   return (
     <div className="dashboard" id="dashboard">
       <EmbedHome />
       <div className="framesContainerHome" id="framesContainerHome">
-        {homeMode === 'FREE' ? (
-          <iframe
-            className="homes-iframe"
-            src="https://panorama-home-pages.s3.amazonaws.com/panorama-free-home.html"
-          ></iframe>
-        ) : homeMode === 'SAAS' ? (
-          <iframe
-            className="homes-iframe"
-            src="https://panorama-home-pages.s3.amazonaws.com/panorama-saas-home.html"
-          ></iframe>
-        ) : homeMode === 'CUSTOM' ? (
-          <iframe
-            className="homes-iframe"
-            src="https://panorama-home-pages.s3.amazonaws.com/panorama-custom-home.html"
-          ></iframe>
-        ) : (
-          <iframe
-            className="homes-iframe"
-            src="https://panorama-home-pages.s3.amazonaws.com/panorama-demo-home.html"
-          ></iframe>
-        )}
+        <iframe className="homes-iframe" src={iframeSrc} title="Home Mode"></iframe>
       </div>
     </div>
   );
