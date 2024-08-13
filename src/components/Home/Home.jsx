0729c187cfca4e04d39c7beee390db4b9c1d8269
superlucky19971023 <superlucky19971023@gmail.com>
2024-08-13 15:46:17 -0300

    Replacement by a single home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,16 +5,18 @@ import EmbedHome from './EmbedHome';
 const Home = () => {
   const { homeMode } = useContext(DashboardTypeContext);
 
-  let iframeSrc;
-  if (homeMode === 'FREE') {
-    iframeSrc = 'https://panorama-home-pages.s3.amazonaws.com/panorama-free-home.html';
-  } else if (homeMode === 'SAAS') {
-    iframeSrc = 'https://panorama-home-pages.s3.amazonaws.com/panorama-saas-home.html';
-  } else if (homeMode === 'CUSTOM') {
-    iframeSrc = 'https://panorama-home-pages.s3.amazonaws.com/panorama-custom-home.html';
-  } else {
-    iframeSrc = 'https://panorama-home-pages.s3.amazonaws.com/panorama-demo-home.html';
-  }
+  // let iframeSrc;
+  // if (homeMode === 'FREE') {
+  //   iframeSrc = 'https://panorama-home-pages.s3.amazonaws.com/panorama-free-home.html';
+  // } else if (homeMode === 'SAAS') {
+  //   iframeSrc = 'https://panorama-home-pages.s3.amazonaws.com/panorama-saas-home.html';
+  // } else if (homeMode === 'CUSTOM') {
+  //   iframeSrc = 'https://panorama-home-pages.s3.amazonaws.com/panorama-custom-home.html';
+  // } else {
+  //   iframeSrc = 'https://panorama-home-pages.s3.amazonaws.com/panorama-demo-home.html';
+  // }
+
+  let iframeSrc = 'https://panorama-home-pages.s3.amazonaws.com/panorama-home.html';
 
   return (
     <div className="dashboard" id="dashboard">
