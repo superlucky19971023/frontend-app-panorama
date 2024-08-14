95b5586e550cbe4d9e55af253cb84d693a9900a8
superlucky19971023 <superlucky19971023@gmail.com>
2024-08-14 09:41:33 -0300

    Fix embed Homes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,26 +1,48 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { AppContext } from '@edx/frontend-platform/react';
 import { DashboardTypeContext } from '../DashboardContext';
-import EmbedHome from './EmbedHome';
+import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
+import { camelCaseObject } from '@edx/frontend-platform';
 
 const Home = () => {
   const { homeMode } = useContext(DashboardTypeContext);
 
-  // let iframeSrc;
-  // if (homeMode === 'FREE') {
-  //   iframeSrc = 'https://panorama-home-pages.s3.amazonaws.com/panorama-free-home.html';
-  // } else if (homeMode === 'SAAS') {
-  //   iframeSrc = 'https://panorama-home-pages.s3.amazonaws.com/panorama-saas-home.html';
-  // } else if (homeMode === 'CUSTOM') {
-  //   iframeSrc = 'https://panorama-home-pages.s3.amazonaws.com/panorama-custom-home.html';
-  // } else {
-  //   iframeSrc = 'https://panorama-home-pages.s3.amazonaws.com/panorama-demo-home.html';
-  // }
+  const {
+    changeHomeMode, changeError,
+  } = useContext(DashboardTypeContext);
+  const { config } = useContext(AppContext);
+  const [modeHome, setModeHome] = useState();
+  const [iframeSrc, setIframeSrc] = useState();
 
-  let iframeSrc = 'https://panorama-home-pages.s3.amazonaws.com/panorama-home.html';
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const url = `${config.LMS_BASE_URL}/panorama/api/get-panorama-mode`;
+        const { data } = await getAuthenticatedHttpClient().get(url);
+        const enrollmentData = camelCaseObject(data);
+        const home = enrollmentData.body;
+        setModeHome(home);
+        changeHomeMode(modeHome);
+      } catch (error) {
+        const httpErrorStatus = error?.response?.status;
+        changeError(httpErrorStatus);
+      }
+    };
+    fetchData();
+
+    if (homeMode === 'FREE') {
+      setIframeSrc('https://panorama-home-pages.s3.amazonaws.com/panorama-free-home.html');
+    } else if (homeMode === 'SAAS') {
+      setIframeSrc('https://panorama-home-pages.s3.amazonaws.com/panorama-saas-home.html');
+    } else if (homeMode === 'CUSTOM') {
+      setIframeSrc('https://panorama-home-pages.s3.amazonaws.com/panorama-custom-home.html');
+    } else {
+      setIframeSrc('https://panorama-home-pages.s3.amazonaws.com/panorama-demo-home.html');
+    }
+  }, [config.LMS_BASE_URL]);
 
   return (
     <div className="dashboard" id="dashboard">
-      <EmbedHome />
       <div className="framesContainerHome" id="framesContainerHome">
         <iframe className="homes-iframe" src={iframeSrc} title="Home Mode"></iframe>
       </div>
