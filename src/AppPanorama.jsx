736a6dc5c4ff88fd9c805efac5836473a02893b7
superlucky19971023 <superlucky19971023@gmail.com>
2024-08-14 15:28:14 -0300

    fix code

--- src/AppPanorama.jsx
+++ src/AppPanorama.jsx
@@ -3,19 +3,17 @@ import { DashboardTypeProvider } from './components/DashboardContext';
 import Panels from './components/Panels/Panels';
 import Home from './components/Home/Home';
 
-const AppPanorama = () => {
-  return (
-    <main>
-      <div className="content-panorama">
-        <DashboardTypeProvider>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/panels" element={<Panels />} />
-          </Routes>
-        </DashboardTypeProvider>
-      </div>
-    </main>
-  );
-};
+const AppPanorama = () => (
+  <main>
+    <div className="content-panorama">
+      <DashboardTypeProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/panels" element={<Panels />} />
+        </Routes>
+      </DashboardTypeProvider>
+    </div>
+  </main>
+);
 
 export default AppPanorama;
